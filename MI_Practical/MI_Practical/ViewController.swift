fa5a5ea60a4fb0d88699b73b745cd4d414fbe8f3
Mayur Patel <mayur.patel@slscorp.com>
2023-12-19 20:48:41 +0530

    Start Test

---
+++ MI_Practical/MI_Practical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MI_Practical
+//
+//  Created by SLS M2 on 19/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
